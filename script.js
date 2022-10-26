1dbcf328a217ca1eab37c519349c7bd695564e43
gato365 <immanuelwilliams@gato365.com>
2022-10-26 04:17:23 -0700

    Most recent attempts to display current time

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 
 
+var timeDisplayEl = $('#time-display');
+
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
 
