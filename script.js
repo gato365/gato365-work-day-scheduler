fcf85398037dd2829486b1c8a87c010d0fbb8520
gato365 <immanuelwilliams@gato365.com>
2022-10-27 08:30:36 -0700

    Change style of tasks

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 
 
 var timeDisplayEl = $('#time-display');
+var classTask = $('.one-task')
+var allWorkingHours = document.querySelector('.all-tasks');
+let numberHours = 16;
 
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
 
 
 displayTime();
 
+
+for (let j = 9; j <= numberHours; j++) {
+ // Text for Time
+ // textarea for entering task
+ // button to save task
+ 
+document.createElement('div')
+document.createElement('textarea')
+document.createElement('button')
+
+allWorkingHours
+ 
+}
